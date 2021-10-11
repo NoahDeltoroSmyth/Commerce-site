f06cc3be3de733de1150b5652b656e8814f43f00
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-11 15:12:27 -0700

    added 3 products into an array in file motorcycle.js

---
+++ motorcycles.js
@@ -0,0 +1,39 @@
+{/* <div id="1">
+            <img src="./assets/cb360.jpeg" alt="cb360">
+            <p>Manufacturer: Honda</p>
+            <p>Model: CB360</p>
+            <p>Engine: Twin, 356 cc, air cooled</p>
+            <p>Description: A 4-stroke, twin cylinder Motorcycle</p>
+            <p>Price: $1,200</p>
+        </div>
+    </div> */}
+
+export const motorcycles = [
+    {
+        id: '1',
+        img: './assets/cb360.jpeg',
+        manufaturer: 'Honda',
+        model: 'CB360',
+        engine: 'Twin, 356cc, air cooled',
+        description: 'A 4-stroke, twin cylinder vintage motorcyle',
+        price: 1400
+    },
+    {
+        id: '2',
+        img:'./assets/xs650.jpeg',
+        manufaturer: 'Yamaha',
+        model: 'XS650',
+        engine:'Twin, 654cc, air cooled',
+        description:'A 4-stroke, twin cylinder vintage motorcycle',
+        price: 1900
+    },
+    {
+        id: '3',
+        img:'./assets/kz250.jpeg',
+        manufacturer: 'Kawasaki',
+        model: 'KZ250',
+        engine: 'Single, 246cc, air cooled',
+        description: 'A 4-stroke, single cylinder vintage motorcycle',
+        price: 950,
+    }
+];
