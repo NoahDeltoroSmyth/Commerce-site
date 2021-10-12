52a26ecafe24ab6cb35b7100573b2462da37ad54
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-12 16:10:53 -0700

    made render-line-item file and exported to cart

--- Cart/render-cart.js
+++ Cart/render-cart.js
@@ -1,26 +1,13 @@
 import { motorcycles } from '../motorcycles.js';
 import { cartContent } from '../data/cart-data.js';
 import { findById } from '../utils.js';
+import { renderLineItem } from './render-line-items.js';
 
 const tbody = document.getElementById('table-body');
 
 for (let cartItem of cartContent){
     const motorcycleData = findById(cartItem.id, motorcycles);
 
-    const tr = document.createElement('tr');
-
-    const tdProduct = document.createElement('td');
-    tdProduct.textContent = motorcycleData.manufacturer;
-
-    const tdPrice = document.createElement('td');
-    tdPrice.textContent = motorcycleData.price;
-
-    const tdQty = document.createElement('td');
-    tdQty.textContent = cartItem.qty;
-
-    const tdTotal = document.createElement('td');
-    tdTotal.textContent = cartItem.qty * motorcycleData.price;
-
-    tr.append(tdProduct, tdPrice, tdQty, tdTotal);
+    const tr = renderLineItem(cartItem, motorcycleData);
     tbody.appendChild(tr);
 }
