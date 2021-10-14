2ec2a01f14b1643e8225018f2ff3549503d457bb
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-13 17:10:32 -0700

    updated render-cart to accept local storage

--- Cart/render-cart.js
+++ Cart/render-cart.js
@@ -1,17 +1,18 @@
 import { motorcycles } from '../motorcycles.js';
-import { cartContent } from '../data/cart-data.js';
-import { calcOrderTotal, findById } from '../utils.js';
+//import { cartContent } from '../data/cart-data.js';
+import { calcOrderTotal, findById, getCart } from '../utils.js';
 import { renderLineItem } from './render-line-items.js';
 
-const tbody = document.getElementById('table-body');
+const cart = getCart('shoppingcart');
 
-for (let cartItem of cartContent){
+const tbody = document.getElementById('table-body');
+for (let cartItem of cart){
     const motorcycleData = findById(cartItem.id, motorcycles);
 
     const tr = renderLineItem(cartItem, motorcycleData);
     tbody.appendChild(tr);
 }
 
-const orderTotal = calcOrderTotal(cartContent, motorcycles);
+const orderTotal = calcOrderTotal(cart, motorcycles);
 const tdOrderTotal = document.getElementById('total');
 tdOrderTotal.textContent = orderTotal;
