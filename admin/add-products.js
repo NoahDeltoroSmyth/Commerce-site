e4171870a631f7fc06e96babe5028f4deda4a326
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-10-18 15:57:53 -0700

    created add products.js and working eventlistener

---
+++ admin/add-products.js
@@ -0,0 +1,20 @@
+import { addProduct } from '../utils.js';
+
+const form = document.getElementById('product-form');
+
+form.addEventListener('submit', (expect)=> {
+    expect.preventDefault();
+    const data = new FormData(form);
+    const newMoto = {
+        id: data.get('id'),
+        manufacturer: data.get('manufacturer'),
+        model: data.get('model'),
+        img: data.get('img'),
+        engine: data.get('img'),
+        description: data.get('description'),
+        price: Number(data.get('price'))
+
+    };
+    addProduct(newMoto);
+    alert('New Motorcycle has been added');
+});
